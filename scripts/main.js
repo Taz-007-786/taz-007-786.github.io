bf6d82a4b40e860f575dfc863eae671cd3c5aaf3
Taz-007-786 <56346278+Taz-007-786@users.noreply.github.com>
2020-11-27 01:27:24 +0000

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,19 @@
-let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/Sony.png') {
-      myImage.setAttribute ('src','images/PS.gif');
-    } else {
-      myImage.setAttribute ('src','images/Sony.png');
-    }
+function calc(){
+	var qty
+	var cost
+	var displaycost
+	qty=document.getElementById('quantity').value
+	cost=qty*30
+	
+	displaycost= "£" + cost.toFixed(2) //Add a £ to teh beginning and format cost as 2 decimal places
+	document.getElementById('cost').value = displaycost //Write the calculated value to the HTML field "cost" 
+	localStorage.setItem("quantity",qty)
+	localStorage.setItem("cost",displaycost)
+	
+	document.getElementById("soldquantity").innerHTML = localStorage.getItem("quantity");
+	document.getElementById("soldcost").innerHTML = localStorage.getItem("cost");
 }
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Please enter your name. NOW!!');
-  if(!myName || myName === null) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Sony is Best, ' + myName;
-  }
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Sony Is Best, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
+{
+	document.getElementById('vid').play();
 }
